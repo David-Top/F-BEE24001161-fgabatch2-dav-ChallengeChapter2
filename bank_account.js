ad9df5c2445362ec96e19f2216098418588c5cb5
David-Top <david.topanto@students.unnes.ac.id>
2024-06-28 21:20:48 +0700

    fix(bank_account.js): Move class BankAccount to bank_account.js

--- bank_account.js
+++ bank_account.js
@@ -1,47 +1,69 @@
-//Initial balance
-let saldo = 1000000;
-
-//testing balance
-console.log(saldo);
-
-//Function to add balance
-function tambahSaldo() {
-  let sum = Number(
-    window.prompt("Masukan nominal yang ingin Anda setorkan", "")
-  );
-
-  //Handler if amount that inputed is string or negative
-  if (isNaN(sum) || sum < 0) {
-    window.alert("Nominal yang Anda masukan tidak valid");
-
-    //Adding balance with inputed amount
-  } else {
-    saldo = saldo + sum;
-    console.log("Saldo Anda ", saldo);
+export class BankAccount {
+  constructor(amount) {
+    this.amount = amount;
   }
-  document.getElementById("saldo").innerHTML = saldo;
-  return saldo;
+  static balance = 50000;
+
+  //Error Handling
+  static errorHandler() {
+    //For alphabet input
+    if (isNaN(this.amount)) {
+      throw new Error("The amount you enter must be a number");
+    }
+
+    //For negative number input
+    if (this.amount < 0) {
+      throw new Error("The amount you enter mus be positive number");
+    }
+  }
+
+  //Static Method to deposit
+  static deposit() {
+    this.amount = Number(
+      window.prompt("Enter the nominal amount you wish to deposit")
+    );
+    try {
+      BankAccount.errorHandler(this.amount);
+      setTimeout(() => {
+        BankAccount.balance = BankAccount.balance + this.amount;
+        console.log(BankAccount.balance); //testing unit
+        document.getElementById("saldo").innerHTML = formatToIDR(
+          BankAccount.balance
+        );
+      }, 500);
+    } catch (error) {
+      window.alert(error);
+    }
+  }
+
+  //Static method to withdraw
+  static withdraw() {
+    this.amount = Number(
+      window.prompt("Masukan nominal yang ingin Anda ambil")
+    );
+    try {
+      BankAccount.errorHandler(this.amount);
+      //Error handling for amount more than balance
+      if (this.amount > BankAccount.balance) {
+        throw new Error("Your balance is insufficient");
+      }
+      setTimeout(() => {
+        BankAccount.balance = BankAccount.balance - this.amount;
+        console.log(BankAccount.balance); //testing unit
+        document.getElementById("saldo").innerHTML = formatToIDR(
+          BankAccount.balance
+        );
+      }, 500);
+    } catch (error) {
+      window.alert(error);
+    }
+  }
 }
 
-//Function to reduce balance
-function kurangiSaldo() {
-  let sum = Number(window.prompt("Masukan nominal yang ingin Anda ambil", ""));
-  //Handler if balance less than inputed amount
-  if (sum > saldo) {
-    window.alert("Saldo Anda tidak mencukupi");
-
-    //Handler if amount that inputed is string or negative
-  } else if (isNaN(sum) || sum < 0) {
-    window.alert("Nominal yang Anda masukan tidak valid");
-
-    //Reducing balance with inputed amount
-  } else {
-    saldo = saldo - sum;
-    console.log("Saldo Anda ", saldo);
-  }
-  document.getElementById("saldo").innerHTML = saldo;
-  return saldo;
+//function to format number to  IDR(upiah urrency)
+export function formatToIDR(number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(number);
 }
-
-document.getElementById("saldo").innerHTML = saldo;
-// console.log(tambahSaldo(1000));
